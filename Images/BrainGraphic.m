38a8d72c6e5ca811ab95790d0ba74fe1eca5536d
vikweg <vwegmayr@gmail.com>
2015-10-18 23:52:42 +0200

    graphics cont

--- Images/BrainGraphic.m
+++ Images/BrainGraphic.m
@@ -1,6 +1,11 @@
+cables = im2double(imread('Spaghetti.jpg'));
+
 brain = im2double(imread('brain empty.png'));
 brain_gray = im2double(rgb2gray(brain));
 brain_bw = im2bw(brain);
+
+brain_gray = imresize(brain_gray,[size(cables,1), size(cables,2)] );
+brain_bw = imresize(brain_bw,[size(cables,1), size(cables,2)]);
 
 cc = bwconncomp(brain_bw,4);
 
@@ -29,13 +34,21 @@ background(cc2.PixelIdxList{2}) = 0;
 background(cortex_bw) = 0;
 background_color = color_mask(background,250/255,230/255,230/255);
 
-cables = im2double(imread('Spaghetti.jpg'));
-cables = imresize(cables,size(brain_bw));
+% cables = imresize(cables,size(brain_bw));
 cables = mask_rgb(cables,wm);
 
-cables_edge_mask = imdilate(edge(im2double(rgb2gray(cables)),'canny'),strel('disk',1));
+cables = imresize(cables,[round(size(cables,1)/2) round(size(cables,2)/2)]);
+cables = im2double(rgb2gray(cables));
+
+cables_filt = imfilter(cables,fspecial('disk',6));
+cables(cables>0.1) = cables_filt(cables>0.1);
+
+cables_edge_mask = edge(cables,'canny',0.03);
+cables_edge_mask = imdilate(cables_edge_mask,strel('disk',1));
+cables_edge_mask = imresize(cables_edge_mask,size(brain_bw));
+
 cables_edge_mask(logical(imcomplement(wm))) = 0;
-cables_edge_mask(logical(imdilate(cortex_mask,strel('disk',3)))) = 0;
+cables_edge_mask(logical(imdilate(cortex_mask,strel('disk',10)))) = 0;
 
 cables_filling = imcomplement(cables_edge_mask);
 cables_filling(logical(background)) = 0;
@@ -52,11 +65,11 @@ brain = color_mask(cortex_mask,198/255,56/255,32/255);
 % subplot(1,2,2)
 % imshow(brain + cables_edge + cables_filling + background_color)
 
-imwrite(brain + cables_edge + cables_filling + background_color,'cables.png')
-imwrite(brain + background_color + repmat(wm,[1 1 3]),'cortex.png')
+imwrite(brain + background_color + repmat(wm,[1 1 3]),'image_series/1_cortex.png')
+imwrite(brain + cables_edge + cables_filling + background_color,'image_series/2_cables.png')
 
 %% GRID
-N=40;
+N=160;
 n=3;
 nn=1;
 
@@ -100,11 +113,12 @@ end
 
 grid = repmat(grid,[1 1 3]);
 
-imwrite(brain + cables_edge + cables_filling + background_color + grid,'grid.png')
+imwrite(brain + cables_edge + cables_filling + background_color + grid,'image_series/3_grid.png')
+
 
 %% ELLIPSE TEMPLATES
 
-base_ellipse = cell(1,3);
+base_ellipse = cell(1,4);
 for i=1:3
     base_ellipse{i} = zeros(N);
 end
@@ -112,7 +126,7 @@ end
 for e = 1:3  
     for i=1:N
         for j=1:N
-            if abs((N-i+1 - N/2)^2/(10-(e-1)*3)^2 + (j-N/2)^2/(10+(e-1)*3)^2 - 1) <= 0.05 + (e-1)^1.5*0.05
+            if abs((N-i+1 - N/2)^2/(40-(e-1)*12)^2 + (j-N/2)^2/(40+(e-1)*12)^2 - 1) <= 0.05 + (e-1)^1.5*0.05
                 base_ellipse{e}(i,j)=1;
             end      
         end
@@ -136,15 +150,30 @@ for r = 1:2*8+1
     ell_hist{r} = ell_hist{r} / sum(ell_hist{r});
 end
 
+%%
+
+% for e = 1:3
+% base_ellipse{e} = imdilate(base_ellipse{e},strel('disk',2)) - 0.5*base_ellipse{e};
+% end
+% 
+% rot_ell = cell(1,2*8+1);
+% rot_ell{1} = base_ellipse{1};
+% for k=1:2
+%     for r=0:7   
+%         rot_ell{(k-1)*8 + r + 2} = imrotate(base_ellipse{k+1},r*180/8,'nearest','crop');
+%     end
+% end
+% 
 % imwrite(base_ellipse{1},'base1.png')
 % imwrite(base_ellipse{2},'base2.png')
 % imwrite(base_ellipse{3},'base3.png')
 
 %%
 
-base_ellipse{1} = imread('base1.png');
-base_ellipse{2} = imread('base2.png');
-base_ellipse{3} = imread('base3.png');
+base_ellipse{1} = im2double(imread('base1.png'));
+base_ellipse{2} = im2double(imread('base2.png'));
+base_ellipse{3} = im2double(imread('base3.png'));
+base_ellipse{4} = im2double(imread('base0.png'));
 
 rot_ell = cell(1,2*8+1);
 rot_ell{1} = base_ellipse{1};
@@ -153,6 +182,61 @@ for k=1:2
         rot_ell{(k-1)*8 + r + 2} = imrotate(base_ellipse{k+1},r*180/8,'nearest','crop');
     end
 end
+
+%% inset
+
+inset = imread('inset.png');
+inset0 = im2double(inset);
+
+inset0 = imresize(inset0,[600 600]);
+ell_inset = imresize(ell_inset,[600 600]);
+
+% imwrite(inset0,'inset0.png')
+
+imshow(imopen(inset0,strel('disk',5)));
+
+inset1 = inset0;
+inset1(ell_inset~=0) = ell_inset(ell_inset~=0);
+
+%% INSET ANISO
+inset0 = imread('inset0.png');
+
+inset0 = imopen(inset0,strel('disk',2));
+
+inset0 = imresize(inset0,[600 600]);
+
+imwrite(inset0,'inset00.png')
+
+ell_inset = imread('base3.png');
+ell_inset = imresize(ell_inset,[500 500]);
+ell_inset = imopen(ell_inset,strel('disk',10));
+ell_inset = padarray(ell_inset,[50 50]);
+
+inset1 = inset0;
+inset1(ell_inset>0) = ell_inset(ell_inset>0);
+
+imwrite(inset1,'inset1.png')
+
+%% INSET ISO
+
+inset0 = imread('inset_iso_00.png');
+
+inset0 = imopen(inset0,strel('disk',2));
+
+% inset0 = imresize(inset0,[600 600]);
+
+imwrite(inset0,'inset_iso_01.png')
+
+ell_inset = imread('base0.png');
+ell_inset = imresize(ell_inset,[600 600]);
+ell_inset = imopen(ell_inset,strel('disk',10));
+% ell_inset = padarray(ell_inset,[50 50]);
+
+inset1 = inset0;
+inset1(ell_inset>0) = ell_inset(ell_inset>0);
+
+imwrite(inset1,'inset_iso_02.png')
+
 
 %% ELLIPSE DATA
 
@@ -169,8 +253,9 @@ while i+N-1 < size(cortex_bw,1)
     
     cortex_count = sum(sum(double(cortex_bw(i:i+N-1,j:j+N-1))));
     cable_count = sum(sum(double(cables_edge_mask(i:i+N-1,j:j+N-1))));
+    wm_count = sum(sum(double(wm(i:i+N-1,j:j+N-1))));
     
-    if (cortex_count > 10*N || cable_count == 0) && cortex_count > 0
+    if cortex_count > N^2/10 || (cable_count == 0 && cortex_count > 0)
         ell_grid(i:i+N-1,j:j+N-1,1:3) = rot_ell{1};
     elseif sum(sum(double(block))) > 0
         
@@ -184,7 +269,11 @@ while i+N-1 < size(cortex_bw,1)
             kldiv_new = KLdiv(ell_hist{r},block_hist);
             if kldiv_new < kldiv
                 kldiv = kldiv_new;
+                if r== 1 && cortex_count < N^2/10
+                ell_grid(i:i+N-1,j:j+N-1,1:3) = base_ellipse{4}; 
+                else
                 ell_grid(i:i+N-1,j:j+N-1,1:3) = rot_ell{r};
+                end
             end
         end
         
@@ -196,15 +285,114 @@ end
 
 % ell_grid = [ell_grid(:,t:end) ell_grid(:,1:t-1)];
 
-imshow(ell_grid)
+% imshow(ell_grid)
 
 %%
-imshow(repmat(-1*ell_grid,[1 1 3]) + 1*brain + 0*cables_edge + 0*cables_filling + background_color + grid + repmat(wm,[1 1 3]))
+
+cables_edge = color_mask(cables_edge_mask,230/255,200/255,200/255);
+brain = color_mask(cortex_mask,210/255,170/255,170/255);
+
+img_tot = 1*brain + cables_edge + 1*cables_filling + background_color + grid;
+img_tot(ell_grid~=0) = ell_grid(ell_grid~=0);
+imwrite(img_tot,'image_series/5_ellgrid.png')
+
+
+
 % imshow(repmat(-1*ell_grid,[1 1 3]) + brain + 0.5*cables_edge + 1*cables_filling + background_color + grid)
 
+%% INSET ISO
+
+img_tot = 1*brain + cables_edge + 1*cables_filling + background_color + grid;
+
+inset0 = imread('inset_iso_01.png');
+
+d=150;
+img_tot(d:d+599,size(img_tot,2)-599-d:size(img_tot,2)-d,1:3) = im2double(inset0);
+
+marked0 = imread('inset_iso.png');
+p=6;
+marked0 = padarray(marked0,[p p]);
+img_tot(7*(N+2*n)-p: 7*(N+2*n) + size(marked0,1)-1-p,22*(N+2*n)-p: 22*(N+2*n) + size(marked0,1)-1-p,1:3) = im2double(marked0);
+
+imwrite(img_tot,'image_series/4_inset.png')
 
 
+%% INSET ANISO
 
+img_tot = 1*brain + cables_edge + 1*cables_filling + background_color + grid;
+
+inset0 = imread('inset00.png');
+
+d=150;
+img_tot( d:d+size(inset0,1)-1 , size(img_tot,2)-size(inset0,2)+1-d:size(img_tot,2)-d , 1:3) = im2double(inset0);
+
+marked0 = imread('inset.png');
+p=6;
+marked0 = padarray(marked0,[p p]);
+img_tot(6*(N+2*n)-p: 6*(N+2*n) + size(marked0,1)-1-p,20*(N+2*n)-p: 20*(N+2*n) + size(marked0,1)-1-p,1:3) = im2double(marked0);
+
+imwrite(img_tot,'image_series/5a_inset.png')
+
+%% INSET ANISO ellipse
+
+img_tot = 1*brain + cables_edge + 1*cables_filling + background_color + grid;
+
+inset0 = imread('frame_aniso_ell.png');
+
+d=150;
+img_tot( d:d+size(inset0,1)-1 , size(img_tot,2)-size(inset0,2)+1-d:size(img_tot,2)-d , 1:3) = im2double(inset0);
+
+marked0 = imread('marked_aniso_ell.png');
+p=6;
+marked0 = padarray(marked0,[p p]);
+img_tot(6*(N+2*n)-p: 6*(N+2*n) + size(marked0,1)-1-p,20*(N+2*n)-p: 20*(N+2*n) + size(marked0,1)-1-p,1:3) = im2double(marked0);
+
+imwrite(img_tot,'image_series/5c_inset.png')
+
+%% INSET ISO ellipse
+
+img_tot = 1*brain + cables_edge + 1*cables_filling + background_color + grid;
+
+inset0 = imread('frame_iso_ell.png');
+
+d=150;
+img_tot(d:d+599,size(img_tot,2)-599-d:size(img_tot,2)-d,1:3) = im2double(inset0);
+
+marked0 = imread('marked_iso_ell.png');
+p=6;
+marked0 = padarray(marked0,[p p]);
+img_tot(7*(N+2*n)-p: 7*(N+2*n) + size(marked0,1)-1-p,22*(N+2*n)-p: 22*(N+2*n) + size(marked0,1)-1-p,1:3) = im2double(marked0);
+
+imwrite(img_tot,'image_series/5d_inset.png')
+
+
+%%
+
+for i = 1:size(inset0,1)
+    for j = 1:size(inset0,2)
+        if inset0(i,j,1) > 140 && inset0(i,j,2) < 200 && inset0(i,j,3) < 150 || (inset0(i,j,1) > 200 && inset0(i,j,2) < 200)
+        inset0(i,j,1:3) = [230 200 200];
+        end
+    end
+end
+
+imshow(inset0)
+
+imwrite(inset0,'frame_iso_ell.png')
+
+%%
+
+for i = 1:size(marked0,1)
+    for j = 1:size(marked0,2)
+        if marked0(i,j,1) > 140 && marked0(i,j,2) < 200 && marked0(i,j,3) < 150 || (marked0(i,j,1) > 200 && marked0(i,j,2) < 200)
+        marked0(i,j,1:3) = [230 200 200];
+        end
+    end
+end
+
+imshow(marked0)
+
+imwrite(marked0,'marked_aniso_ell.png')
 
 
 
